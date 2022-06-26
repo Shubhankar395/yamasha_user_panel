ffe9a728c5decf833fceaf18d5bb58af8d6d2840
Rajeadarsh15 <80952515+Rajeadarsh15@users.noreply.github.com>
2022-06-26 16:03:04 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,7 +4,7 @@
 if(location.host=='192.168.100.5'){
     var api_base ='http://192.168.100.5/yamasha_v2/api/user_panel/'
   }else{
-    var api_base ='http://192.168.100.5/yamasha_v2/api/user_panel/'
+    var api_base ='https://yamasha.in/api/user_panel/'
   }
 
 //   console.log(api_base)
