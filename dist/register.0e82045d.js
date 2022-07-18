4407ff54810dee411287292bad1cef07b7d03d13
Rajeadarsh15 <80952515+Rajeadarsh15@users.noreply.github.com>
2022-07-19 01:11:20 +0530

    some fixes
     added some checks and 5 min sleep time

--- dist/register.0e82045d.js
+++
@@ -1,8 +0,0 @@
-if (location.host == "192.168.100.5") var api_base = "http://192.168.100.5/yamasha_v2/api/user_panel/";
-else var api_base = "https://api.yamasha.in/user_panel/";
-//   console.log(api_base)
-/// asset version control variable
-// yamasha global assets version
-y_g_a_v = 1;
-
-//# sourceMappingURL=register.0e82045d.js.map
